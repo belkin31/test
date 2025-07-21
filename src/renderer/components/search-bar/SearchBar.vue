d5b888a146d2310dbb508e824ad9cbd07e8ae7be
Artem Belkin <artem.belkin@enterra-inc.com>
2025-07-21 22:59:43 +0700

    upgrade catalog

---
+++ src/renderer/components/search-bar/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <input
+      type="text"
+      class="search-bar"
+      v-model="query"
+      @input="$emit('onSearch', query)"
+      :placeholder="placeholderAttr"
+  />
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  props: {
+    placeholderAttr: {
+      type: String,
+      default: 'Поиск товара...'
+    }
+  },
+  emits: ['search'],
+  data() {
+    return { query: '' };
+  }
+};
+</script>
+<style src="./search-bar.scss"/>
